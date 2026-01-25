e4f60a23625984141f549d7604002a8c21e30d9f
Wensellg <wensellg9676@connect.durhamtech.edu>
2026-01-25 15:19:55 -0500

    Update numeric type conversion exercise

--- Unit1/Lesson1_4/Lab_Operators.playground/Pages/7. Exercise - Numeric Type Conversion.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_4/Lab_Operators.playground/Pages/7. Exercise - Numeric Type Conversion.xcplaygroundpage/Contents.swift
@@ -1,16 +1,17 @@
 /*:
-## Exercise - Numeric Type Conversion
+ ## Exercise - Numeric Type Conversion
+*/
 
- Create an integer constant `x` with a value of 10, and a double constant `y` with a value of 3.2. Create a constant `multipliedAsIntegers` equal to `x` times `y`. Does this compile? If not, fix it by converting your `Double` to an `Int` in the mathematical expression. Print the result.
- */
+let x = 10
+let y = 3.2
 
+// Convert Double → Int
+let multipliedAsIntegers = x * Int(y)
+print(multipliedAsIntegers)
 
-//:  Create a constant `multipliedAsDoubles` equal to `x` times `y`, but this time convert the `Int` to a `Double` in the expression. Print the result.
+// Convert Int → Double
+let multipliedAsDoubles = Double(x) * y
+print(multipliedAsDoubles)
 
-
-//:  Are the values of `multipliedAsIntegers` and `multipliedAsDoubles` different? Print a statement to the console explaining why.
-
-
-/*:
-[Previous](@previous)  |  page 7 of 8  |  [Next: App Exercise - Converting Types](@next)
- */
+// Explanation
+print("They differ because converting y to Int removes the decimal, while converting x to Double preserves the fractional value.")
