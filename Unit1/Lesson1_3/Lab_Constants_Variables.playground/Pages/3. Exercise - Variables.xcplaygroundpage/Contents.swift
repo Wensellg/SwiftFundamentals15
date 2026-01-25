76154f6fdcaa2bb3335959091f297d2337b2bf63
Wensellg <wensellg9676@connect.durhamtech.edu>
2026-01-24 23:01:18 -0500

    Update variable exercise and explanations

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -1,16 +1,21 @@
 /*:
-## Exercise - Variables
+ ## Exercise - Variables
  
  Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
  */
 
+var schooling = 14
+print(schooling)
 
-//:  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
+//: Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
 
+schooling += 1
+print(schooling)
 
-//:  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
+//: Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
 
+print("Yes, the code compiles because `schooling` is a variable, which means its value can be changed. This is different from a constant, which cannot be updated after it is set.")
 
 /*:
-[Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
+ [Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
  */
