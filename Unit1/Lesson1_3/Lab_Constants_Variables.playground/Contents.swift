fa86ff57dd55b41f9fbdb79625ae64d8faf790fd
Wensellg <wensellg9676@connect.durhamtech.edu>
2026-01-25 14:59:21 -0500

    Update playground with constants and variables examples

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
@@ -1,50 +1,69 @@
 /*:
  ## Exercise - Constants
- 
- Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
- */
+*/
 
-
-/*:
- Now assume you go through and remove a lot of your friends that aren't active on social media. Update your `friends` constant to a lower number than it currently is between 1 and 900.
- */
-
+// Declare a constant and print it
+let friends = 250
+print(friends)
 
 /*:
- Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friend` constant to a lower number so that the playground will compile properly.
- */
-
-
-/*:
- Declare a variable `age` and set it to your own age. Print `age` to the console.
- */
-
+ Updating a constant (this line should be deleted because it will NOT compile)
+ friends = 150
+*/
 
 /*:
- Now pretend you just had a birthday, and update the `age` variable accordingly. Print `age` to the console.
- */
+ Explanation of why it doesn't compile
+*/
+print("The code does not compile because 'friends' is a constant and constants cannot be changed after they are set.")
 
 /*:
- 
- */
-
-
-/*:
- Create a double variable with a value of 1.1. Update it to 2.2, 3.3, and 4.4, printing out the value after each assignment (again by referencing the variable you created).
- */
-
+ Declare a variable `age` and print it
+*/
+var age = 34
+print(age)
 
 /*:
- Create a boolean variable and set it to `true`. Print the variable, then assign it a value of `false`, and print it again.
- */
-
-
-/*:
- Create two variables, one with a value of 0, the other with a value of 0.0. Try to assign the second variable to the first, and you will receive an error. Add the necessary type annotation that will allow the second variable to be assigned to the first.
- */
-
+ Update `age` after a birthday
+*/
+age += 1
+print(age)
 
 /*:
- Create a variable integer with a value of 1,000,000,000, ensuring that you format it so it is more readable (i.e. it's hard to read 1000000000, so make it easier to read).
- */
+ Create a double variable and update it several times
+*/
+var myDouble = 1.1
+print(myDouble)
 
+myDouble = 2.2
+print(myDouble)
+
+myDouble = 3.3
+print(myDouble)
+
+myDouble = 4.4
+print(myDouble)
+
+/*:
+ Boolean variable
+*/
+var isActive = true
+print(isActive)
+
+isActive = false
+print(isActive)
+
+/*:
+ Type annotation to allow assignment
+*/
+var intZero: Int = 0
+var doubleZero = 0.0
+
+// intZero = doubleZero   // error without type conversion
+intZero = Int(doubleZero)
+print(intZero)
+
+/*:
+ Readable large integer
+*/
+var bigNumber = 1_000_000_000
+print(bigNumber)
