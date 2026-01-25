198b83aa79f388fb38562ffe668a44e529715dfc
Wensellg <wensellg9676@connect.durhamtech.edu>
2026-01-24 23:00:35 -0500

    Update Contents.swift

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -1,16 +1,21 @@
 /*:
-## Exercise - Constants
+ ## Exercise - Constants
 
  Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
  */
 
+let friends = 250
+print(friends)
 
-//:  Now assume you go through and remove friends that aren't active on social media. Attempt to update your `friends` constant to a lower number than it currently is. Observe what happens and then move to the next step.
-
-
-//:  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
-
+//: Now assume you go through and remove friends that aren't active on social media.
+//: Attempting to update the `friends` constant would cause an error, so that line has been removed.
 
 /*:
-page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
+ Does the above code compile? Why not? Print your explanation to the console using the `print` function.
+ */
+
+print("The code does not compile if you try to change `friends` because it is a constant, and constants cannot be updated after they are set.")
+
+/*:
+ page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
  */
