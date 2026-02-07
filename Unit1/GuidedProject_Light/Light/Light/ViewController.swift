3aa261d18db5ebd5cab1d559880cb2abc28c753c
Wensellg <wensellg9676@connect.durhamtech.edu>
2026-02-07 11:56:52 -0500

    Simplify updateUI method implementation
    Refactor updateUI method to use a ternary operator for background color.

--- Unit1/GuidedProject_Light/Light/Light/ViewController.swift
+++ Unit1/GuidedProject_Light/Light/Light/ViewController.swift
@@ -2,7 +2,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var lightButton: UIButton!
     var lightOn = true
 
     override func viewDidLoad() {
@@ -10,14 +9,8 @@ class ViewController: UIViewController {
         updateUI()
     }
 
-    fileprivate func updateUI() {
-        if lightOn {
-            view.backgroundColor = .white
-            lightButton.setTitle("Off", for: .normal)
-        } else {
-            view.backgroundColor = .black
-            lightButton.setTitle("On", for: .normal)
-        }
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
     }
 
     @IBAction func buttonPressed(_ sender: Any) {
