896b2bdf35f94a39132247a3ba61ab7dc431212f
Wensellg <wensellg9676@connect.durhamtech.edu>
2026-02-07 11:55:53 -0500

    Implement button action to toggle light and background color
    Added buttonPressed action to toggle light state and change background color.

--- Unit1/GuidedProject_Light/Light/Light/ViewController.swift
+++ Unit1/GuidedProject_Light/Light/Light/ViewController.swift
@@ -1,1 +1,9 @@
-var lightOn = true
+@IBAction func buttonPressed(_ sender: Any) {
+    lightOn.toggle()
+
+    if lightOn {
+        view.backgroundColor = .white
+    } else {
+        view.backgroundColor = .black
+    }
+}
