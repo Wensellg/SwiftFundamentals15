2b29bc4e304ff2ba96211ca617c0fac5654883be
Wensellg <wensellg9676@connect.durhamtech.edu>
2026-02-07 11:56:12 -0500

    Refactor ViewController to improve light toggle logic

--- Unit1/GuidedProject_Light/Light/Light/ViewController.swift
+++ Unit1/GuidedProject_Light/Light/Light/ViewController.swift
@@ -1,9 +1,27 @@
-@IBAction func buttonPressed(_ sender: Any) {
-    lightOn.toggle()
+import UIKit
 
-    if lightOn {
-        view.backgroundColor = .white
-    } else {
-        view.backgroundColor = .black
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lightButton: UIButton!
+    var lightOn = true
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    fileprivate func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+            lightButton.setTitle("Off", for: .normal)
+        } else {
+            view.backgroundColor = .black
+            lightButton.setTitle("On", for: .normal)
+        }
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
     }
 }
