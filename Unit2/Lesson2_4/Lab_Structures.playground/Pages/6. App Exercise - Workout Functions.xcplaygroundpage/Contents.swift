c9a94b9ea7a5c1c19be7f4803b58abe554b7b992
Wensellg <wensellg9676@connect.durhamtech.edu>
2026-02-14 13:29:15 -0500

    Add postWorkoutStats and takeStep methods

--- Unit2/Lesson2_4/Lab_Structures.playground/Pages/6. App Exercise - Workout Functions.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_4/Lab_Structures.playground/Pages/6. App Exercise - Workout Functions.xcplaygroundpage/Contents.swift
@@ -10,16 +10,27 @@ struct RunningWorkout {
     var time: Double
     var elevation: Double
     
+    func postWorkoutStats() {
+        print("You ran \(distance) meters in \(time) minutes and gained \(elevation) meters of elevation.")
+    }
 }
-
+let firstRun = RunningWorkout(distance: 2396, time: 15.3, elevation: 94)
+firstRun.postWorkoutStats()
 
 //:  A `Steps` struct has been created for you below, representing the day's step-tracking data. It has the goal number of steps for the day and the number of steps taken so far. Create a method on `Steps` called `takeStep` that increments the value of `steps` by one. Then create an instance of `Steps` and call `takeStep()`. Print the value of the instance's `steps` property before and after the method call.
 struct Steps {
     var steps: Int
     var goal: Int
     
+    mutating func takeStep() {
+        steps += 1
+    }
 }
+var today = Steps(steps: 9999, goal: 10000)
 
+print("Steps before: \(today.steps)")
+today.takeStep()
+print("Steps after: \(today.steps)")
 
 /*:
 [Previous](@previous)  |  page 6 of 10  |  [Next: Exercise - Computed Properties and Property Observers](@next)
