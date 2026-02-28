a75cb3c348644349c9b55785d79f41406d484124
Wensellg <wensellg9676@connect.durhamtech.edu>
2026-02-27 22:43:40 -0500

    Update ViewController.swift

--- Unit2/Lesson2_10/Two_Buttons/ViewController.swift
+++ Unit2/Lesson2_10/Two_Buttons/ViewController.swift
@@ -1,1 +1,20 @@
+import UIKit
 
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+
+    @IBAction func setTextButtonTapped(_ sender: UIButton) {
+        label.text = textField.text
+    }
+
+    @IBAction func clearTextButtonTapped(_ sender: UIButton) {
+        textField.text = ""
+        label.text = ""
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
