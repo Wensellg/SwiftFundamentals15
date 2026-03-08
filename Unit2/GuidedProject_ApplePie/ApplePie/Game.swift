16f07d6a4bbb4b10134b9738a8b218e3057e0cd5
Wensellg <wensellg9676@connect.durhamtech.edu>
2026-03-07 23:21:25 -0500

    Update Game.swift

--- Unit2/GuidedProject_ApplePie/ApplePie/Game.swift
+++ Unit2/GuidedProject_ApplePie/ApplePie/Game.swift
@@ -1,10 +1,17 @@
+import Foundation
+
 struct Game {
     var word: String
     var incorrectMovesRemaining: Int
-    var guessedLetters: [Character]
+    private(set) var guessedLetters: [Character] = []
 
-    mutating func playerGuessed(letter: Character) {
+    var formattedWord: String {
+        word.map { guessedLetters.contains($0) ? $0 : "_" }.map(String.init).joined()
+    }
+
+    mutating func playerGuessed(_ letter: Character) {
         guessedLetters.append(letter)
+
         if !word.contains(letter) {
             incorrectMovesRemaining -= 1
         }
