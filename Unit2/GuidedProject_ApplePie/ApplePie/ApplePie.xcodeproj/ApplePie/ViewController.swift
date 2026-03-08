c2968f4699e7cb295b31c71b00242d615eebb6bc
Wensellg <wensellg9676@connect.durhamtech.edu>
2026-03-07 23:07:29 -0500

    Add IBOutlet and IBAction for UI components

--- Unit2/GuidedProject_ApplePie/ApplePie/ApplePie.xcodeproj/ApplePie/ViewController.swift
+++ Unit2/GuidedProject_ApplePie/ApplePie/ApplePie.xcodeproj/ApplePie/ViewController.swift
@@ -1,1 +1,13 @@
 var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
+@IBOutlet var treeImageView: UIImageView!
+@IBOutlet var correctWordLabel: UILabel!
+@IBOutlet var scoreLabel: UILabel!
+@IBOutlet var letterButtons: [UIButton]!
+@IBAction func letterButtonPressed(_ sender: UIButton) {
+    sender.isEnabled = false
+}
+var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
+let incorrectMovesAllowed = 7
+
+var totalWins = 0
+var totalLosses = 0
